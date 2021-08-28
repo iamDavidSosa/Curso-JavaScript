e21d33390cf4ee6c39e99c4776aa8de24d690399
Anderson David Sosa <a.davidsosa.t@gmail.com>
2021-08-27 22:11:25 -0400

    Agregamos los tipos de variables y los operadores de asignacion

---
+++ JavaScript.js
@@ -0,0 +1,16 @@
+//Variables en Javascript
+var string = "Cadena de texto"; //La palabra var le da un alcance global
+var number;
+var boolean = true;
+var symbol = 1;
+
+let condicion = 1; //La palabra let le da un alcance dentro del bloque, funcion o conditional
+
+const pi = 3.141592653589; //La palabra const declara una constante
+
+//Funcion prompt
+
+let nombre = prompt("Ingresa tu nombre: ");
+alert("Bienvenido, "+nombre);
+
+document.write(nombre);
