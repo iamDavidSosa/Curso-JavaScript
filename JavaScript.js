137bdf422aa56a4877a2cf461f5e94876e1885e4
Anderson David Sosa <a.davidsosa.t@gmail.com>
2021-08-27 22:33:11 -0400

    Agregamos la concatenacion de texto

--- JavaScript.js
+++ JavaScript.js
@@ -14,3 +14,25 @@ let nombre = prompt("Ingresa tu nombre: ");
 alert("Bienvenido, "+nombre);
 
 document.write(nombre);
+
+//Operadores de asignacion
+
+let numero = 10;
+
+numero += 5;
+
+numero -= 2;
+
+numero *= 3;
+
+numero %= 4; //Divide el valor de numero entre 4 y toma el valor del resto de
+
+numero **= 2; //Eleva el valor de numero a 2
+
+//Concatenar texto 
+
+let saludo = "Hola David!";
+let pregunta = "Como estas?"
+
+let oracion = saludo +" "+ pregunta;
+alert(oracion);
