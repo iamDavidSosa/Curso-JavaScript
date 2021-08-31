ffbc3de10056b3c75f03e25f7889da23b7462fb4
Anderson David Sosa <a.davidsosa.t@gmail.com>
2021-08-31 11:34:08 -0400

    Agregamos los metodos math en el archivo metodo-math.js

---
+++ metodo-math.js
@@ -0,0 +1,27 @@
+let numero;
+
+numero = Math.sqrt(25); //Devuelve la raiz cuadrada de un numero
+
+numero = Math.cbrt(27); //Devuelve la raiz cubica de un numero
+
+numero = Math.max(1, 2, 3, 5, 40, 4, 132, 90, 12, 7); //Retorna el numero mas grande
+
+numero = Math.min(1, 2, 3, 5, 40, 4, 132, 90, 12, 7); //Retorna el numero mas pequeño
+
+
+numero = Math.random()*100; //Devuelve un numero random entre 0 y 1, nunca llega ni a cero ni a uno
+
+numero = Math.round(numero); //Redondea un decimal al entero mas cercano
+
+numero = Math.floor(2.999); //Redondea para abajo 
+
+numero = Math.trunc(9.9999); //Elimina los valores decimales
+
+numero = Math.PI; //Retorna PI
+
+numero = Math.SQRT1_2; //Retorna la raiz cuadrada de 1/2
+
+numero = Math.SQRT2; //Retorna la raiz cuadrada de 2
+
+document.write(numero);
+ 
