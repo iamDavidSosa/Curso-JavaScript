508cdb9ed5f30832f86833aab379b46fb4dde15b
Anderson David Sosa <a.davidsosa.t@gmail.com>
2021-08-31 12:09:51 -0400

    Agregamos los metodos de consola en metodos-consola.js

---
+++ metodos-consola.js
@@ -0,0 +1,36 @@
+//      Funciones de Registro   //
+
+console.clear(); //Limpia la consola
+
+console.log("Hola"); //Muestra un mensaje en la consola
+
+console.info("Informacion"); //Muestra un mensaje en consola casi igual que el .log
+
+console.table([[12,12,12,323,34,12,,45,5,34,23]]); //Crea una tabla solo con un array o un objeto
+
+console.warn("Che capo, andate con cuidado noma..."); //Muestra una advertencia en consola
+
+
+
+//      Funciones de Conteo     //
+
+console.count(); //Esto hace un conteo de cuantas veces se ejecuta esta misma linea
+
+console.countReset(); //Esto resetea la cuenta del count
+
+
+
+
+//      Funciones de Agrupacion     //
+
+console.group("Registro usuario"); //Crea un grupo en donde se registrara la consola
+
+console.groupEnd(); //Cierra o termina el grupo
+
+console.groupCollapsed("Registro"); //Crea un grupo igual que .group, pero no lo despliega
+
+
+//      Funciones de Temporizacion     //   
+
+console.time(); //Inicia un temporizador
+
