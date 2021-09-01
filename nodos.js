1a58a8409b2edca5f026950740ec57b54375d9ec
Anderson David Sosa <a.davidsosa.t@gmail.com>
2021-08-31 21:35:08 -0400

    Creamos nodos.js

---
+++ nodos.js
@@ -0,0 +1,12 @@
+//      DOCUMENT - Metodo de seleccion de elementos     //
+
+document.getElementById(); //Selecciona un elemento por ID
+
+document.getElementsByTagName(); //Selecciona todos los elementos que coincidan con el nombre
+// de la etiqueta especificada
+
+document.querySelector(); //Devuelve el primer elemento que coincida con el grupo especificado
+// de selectores
+
+document.querySelectorAll(); //Devuelve todos los elementos que coincida con el grupo especificado
+// de selectores
