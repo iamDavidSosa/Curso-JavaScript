7d293cddaadd7c16f9b823fff5c23c0c7f486c01
Anderson David Sosa <a.davidsosa.t@gmail.com>
2021-09-02 21:11:36 -0400

    Creamos codigo.js, agregamos mas nodos y cambiamos el tipo de letra

---
+++ codigo.js
@@ -0,0 +1,4 @@
+const titulo = document.querySelector(".titulo");
+
+titulo.setAttribute("title", "titulo normal"); //Oculta el nodo
+
--- nodos.js
+++ nodos.js
@@ -10,3 +10,23 @@ document.querySelector(); //Devuelve el primer elemento que coincida con el grup
 
 document.querySelectorAll(); //Devuelve todos los elementos que coincida con el grupo especificado
 // de selectores
+
+
+//      Metodos para definir, modificar o eliminar los valores de los atributos    //
+
+const rangoEdad = document.querySelector(".rangoEdad");
+
+rangoEdad.setAttribute("type", "password"); //Define o modifica el atributo
+
+rangoEdad.getAttribute("type"); //Retorna el tipo de atributo
+
+rangoEdad.removeAttribute("type"); //Elimina el atributo
+
+
+// Ejemplos de atributos
+
+titulo.setAttribute("contentEditable", "true"); //vuelve al nodo editable
+
+titulo.setAttribute("hidden", "false"); //Oculta el nodo
+
+titulo.setAttribute("title", "titulo normal"); //Le pone una etiqueta que cuando tiene el mouse encima se ve
